a498a5b1a437422320bf6a31e41c6c94baaf75cb
Ivan Afanasyev <ivafanas@gmail.com>
2017-07-11 22:22:25 +0700

    [ ADD ] add tests for object values generation

--- tests/common/value_runner.cpp
+++ tests/common/value_runner.cpp
@@ -1,5 +1,7 @@
 #include "value_runner.h"
 
+#include <string>
+
 
 Json::Value run_make_double_array(int size)
 {
@@ -34,5 +36,37 @@ Json::Value run_make_boolean_array(int size)
     rv.resize(size);
     for (int i = 0; i < size; ++i)
         rv[i] = Json::Value(true);
+    return rv;
+}
+
+Json::Value run_make_object_of_doubles(int size)
+{
+    Json::Value rv(Json::objectValue);
+    for (int i = 0; i < size; ++i)
+        rv[std::to_string(i)] = Json::Value(static_cast<double>(i));
+    return rv;
+}
+
+Json::Value run_make_object_of_integers(int size)
+{
+    Json::Value rv(Json::objectValue);
+    for (int i = 0; i < size; ++i)
+        rv[std::to_string(i)] = Json::Value(i);
+    return rv;
+}
+
+Json::Value run_make_object_of_strings(int size)
+{
+    Json::Value rv(Json::objectValue);
+    for (int i = 0; i < size; ++i)
+        rv[std::to_string(i)] = Json::Value("And now for something completely different");
+    return rv;
+}
+
+Json::Value run_make_object_of_booleans(int size)
+{
+    Json::Value rv(Json::objectValue);
+    for (int i = 0; i < size; ++i)
+        rv[std::to_string(i)] = Json::Value(true);
     return rv;
 }
--- tests/common/value_runner.h
+++ tests/common/value_runner.h
@@ -7,3 +7,8 @@ Json::Value run_make_double_array(int size);
 Json::Value run_make_integer_array(int size);
 Json::Value run_make_string_array(int size);
 Json::Value run_make_boolean_array(int size);
+
+Json::Value run_make_object_of_doubles(int size);
+Json::Value run_make_object_of_integers(int size);
+Json::Value run_make_object_of_strings(int size);
+Json::Value run_make_object_of_booleans(int size);
